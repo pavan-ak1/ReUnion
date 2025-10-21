5512af45a0600521ceb58cb2b388dbfca803590f
pavan-ak1 <kustagipavan30@gmail.com>
2025-10-21 20:40:18 +0530

    Refractored a little code with function

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,21 +1,30 @@
-import dotenv from 'dotenv'
-import express  from 'express'
-import type { Request, Response } from 'express';
+import dotenv from "dotenv";
+import express from "express";
+import type { Request, Response } from "express";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 
-app.get("/", (req:Request,res:Response)=>{
-    res.send("API running in backend");
+//middlewares
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("API running in backend");
 });
-
-
-
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, ()=>{
-    console.log(`Server running on port ${port}`);
-    
-})
+const start = async () => {
+  try {
+    app.listen(port, () => {
+      console.log(`Server running on port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
